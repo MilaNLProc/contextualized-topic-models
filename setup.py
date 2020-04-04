e37ebcedac255b0f585816478fd0f17d5d3f0bc4
vinid <chiccobia@gmail.com>
2020-04-05 00:06:07 +0200

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MilaNLProc/contextualized_topic_models',
-    version='0.4.1',
+    version='0.4.2',
     zip_safe=False,
 )
