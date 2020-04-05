bc911ddf08020b800cdb17046ee7e628fe4d5791
silviatti <s.terragni4@campus.unimib.it>
2020-04-05 13:05:56 +0200

    minor fix

--- contextualized_topic_models/evaluation/measures.py
+++ contextualized_topic_models/evaluation/measures.py
@@ -42,8 +42,9 @@ class CoherenceNPMI(Measure):
         super().__init__()
         self.topics = topics
         self.texts = texts
+        self.dictionary = Dictionary(self.texts)
 
-    def score(self, topk=25):
+    def score(self, topk=10):
         """
         :param topics: a list of lists of the top-k words
         :param texts: (list of lists of strings) represents the corpus on which the empirical frequencies of words are
@@ -54,43 +55,42 @@ class CoherenceNPMI(Measure):
         if topk > len(self.topics[0]):
             raise Exception('Words in topics are less than topk')
         else:
-            dictionary = Dictionary(self.texts)
-            npmi = CoherenceModel(topics=self.topics, texts=self.texts, dictionary=dictionary,
+            npmi = CoherenceModel(topics=self.topics, texts=self.texts, dictionary=self.dictionary,
                                   coherence='c_npmi', topn=topk)
             return npmi.get_coherence()
 
 
 class CoherenceWordEmbeddings(Measure):
-    def __init__(self, topics, texts, word2vec=None, binary=False):
+    def __init__(self, topics, word2vec_path=None, binary=False):
+        '''
+        :param topics: a list of lists of the top-n most likely words
+        :param word2vec_path: if word2vec_file is specified, it retrieves the word embeddings file (in word2vec format) to
+         compute similarities between words, otherwise 'word2vec-google-news-300' is downloaded
+        :param binary: if the word2vec file is binary
+        '''
         super().__init__()
         self.topics = topics
-        self.texts = texts
-        self.word2vec_path = word2vec
         self.binary = binary
+        if word2vec_path is None:
+            self.wv = api.load('word2vec-google-news-300')
+        else:
+            self.wv = KeyedVectors.load_word2vec_format(word2vec_path, binary=binary)
 
     def score(self, topk=10, binary= False):
         """
-        :param topics: a list of lists of the top-n most likely words
         :param topk: how many most likely words to consider in the evaluation
-        :param word2vec_file: if word2vec_file is specified, it retrieves the word embeddings file (in word2vec format) to
-         compute similarities between words, otherwise 'word2vec-google-news-300' is downloaded
-        :param binary: if the word2vec file is binary
         :return: topic coherence computed on the word embeddings similarities
         """
         if topk > len(self.topics[0]):
             raise Exception('Words in topics are less than topk')
         else:
-            if self.word2vec is None:
-                wv = api.load('word2vec-google-news-300')
-            else:
-                wv = KeyedVectors.load_word2vec_format(self.word2vec_path, binary=binary)
             arrays = []
             for index, topic in enumerate(self.topics):
                 if len(topic) > 0:
                     local_simi = []
                     for word1, word2 in itertools.combinations(topic[0:topk], 2):
-                        if word1 in wv.vocab and word2 in wv.vocab:
-                            local_simi.append(wv.similarity(word1, word2))
+                        if word1 in self.wv.vocab and word2 in self.wv.vocab:
+                            local_simi.append(self.wv.similarity(word1, word2))
                     arrays.append(np.mean(local_simi))
             return np.mean(arrays)
 
