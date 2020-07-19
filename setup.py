ef5bfa447b237de0a0c17ae7bd744266dd1e6e20
vinid <chiccobia@gmail.com>
2020-07-19 12:21:50 +0200

    Checking if the BoW and the contexutalized embeddings contain the same number of elements. (see Issue, https://github.com/MilaNLProc/contextualized-topic-models/issues/5)
    Updating reference to package in setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/MilaNLProc/contextualized_topic_models',
+    url='https://github.com/MilaNLProc/contextualized-topic-models',
     version='1.3.1',
     zip_safe=False,
 )
