f7087b7a5269d961051ae814b62397f3fb56805e
vinid <chiccobia@gmail.com>
2020-11-03 12:02:32 +0100

    Bump version: 1.5.1 → 1.5.2

--- contextualized_topic_models/__init__.py
+++ contextualized_topic_models/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Federico Bianchi"""
 __email__ = 'f.bianchi@unibocconi.it'
-__version__ = '1.5.1'
+__version__ = '1.5.2'
--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MilaNLProc/contextualized-topic-models',
-    version='1.5.1',
+    version='1.5.2',
     zip_safe=False,
 )
