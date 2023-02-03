ce6db5d891d720b3416bbe10e4449e05e2e40390
Max Weinzierl <maxwellweinzierl@gmail.com>
2023-02-03 11:07:13 -0600

    Fixed progress bar issues

--- contextualized_topic_models/models/ctm.py
+++ contextualized_topic_models/models/ctm.py
@@ -513,7 +513,7 @@ class CTM:
         )
         final_thetas = []
         with torch.no_grad():
-            for batch_samples in tqdm(loader, position=0, leave=True):
+            for batch_samples in tqdm(loader):
                 # batch_size x vocab_size
                 X_bow = batch_samples["X_bow"]
                 X_bow = X_bow.reshape(X_bow.shape[0], -1)
