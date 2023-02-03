f60f3d994fbe2a179f926ddc9f5b1dbb201cac67
Max Weinzierl <maxwellweinzierl@gmail.com>
2023-02-03 07:47:09 -0600

    Modified CTM to be much more efficient. Flipped sampling and batch loops, then vectorized sampling. Also made generating predictions on all training data optional during fit call, and added new get_doc_topic_distribution_iterator operation for less memory-intensive predictions on large datasets. Also reused mu and logvar for sampling, as they did not need to be recalculated each time.

--- contextualized_topic_models/networks/decoding_network.py
+++ contextualized_topic_models/networks/decoding_network.py
@@ -2,15 +2,26 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from contextualized_topic_models.networks.inference_network import CombinedInferenceNetwork, ContextualInferenceNetwork
+from contextualized_topic_models.networks.inference_network import (
+    CombinedInferenceNetwork,
+    ContextualInferenceNetwork,
+)
 
 
 class DecoderNetwork(nn.Module):
-
-
-    def __init__(self, input_size, bert_size, infnet, n_components=10, model_type='prodLDA',
-                 hidden_sizes=(100,100), activation='softplus', dropout=0.2,
-                 learn_priors=True, label_size=0):
+    def __init__(
+        self,
+        input_size,
+        bert_size,
+        infnet,
+        n_components=10,
+        model_type="prodLDA",
+        hidden_sizes=(100, 100),
+        activation="softplus",
+        dropout=0.2,
+        learn_priors=True,
+        label_size=0,
+    ):
         """
         Initialize InferenceNetwork.
 
@@ -24,14 +35,15 @@ class DecoderNetwork(nn.Module):
         """
         super(DecoderNetwork, self).__init__()
         assert isinstance(input_size, int), "input_size must by type int."
-        assert isinstance(n_components, int) and n_components > 0, \
-            "n_components must be type int > 0."
-        assert model_type in ['prodLDA', 'LDA'], \
-            "model type must be 'prodLDA' or 'LDA'"
-        assert isinstance(hidden_sizes, tuple), \
-            "hidden_sizes must be type tuple."
-        assert activation in ['softplus', 'relu'], \
-            "activation must be 'softplus' or 'relu'."
+        assert (
+            isinstance(n_components, int) and n_components > 0
+        ), "n_components must be type int > 0."
+        assert model_type in ["prodLDA", "LDA"], "model type must be 'prodLDA' or 'LDA'"
+        assert isinstance(hidden_sizes, tuple), "hidden_sizes must be type tuple."
+        assert activation in [
+            "softplus",
+            "relu",
+        ], "activation must be 'softplus' or 'relu'."
         assert dropout >= 0, "dropout must be >= 0."
 
         self.input_size = input_size
@@ -43,15 +55,28 @@ class DecoderNetwork(nn.Module):
         self.learn_priors = learn_priors
         self.topic_word_matrix = None
 
-
         if infnet == "zeroshot":
             self.inf_net = ContextualInferenceNetwork(
-                input_size, bert_size, n_components, hidden_sizes, activation, label_size=label_size)
+                input_size,
+                bert_size,
+                n_components,
+                hidden_sizes,
+                activation,
+                label_size=label_size,
+            )
         elif infnet == "combined":
             self.inf_net = CombinedInferenceNetwork(
-                input_size, bert_size, n_components, hidden_sizes, activation, label_size=label_size)
+                input_size,
+                bert_size,
+                n_components,
+                hidden_sizes,
+                activation,
+                label_size=label_size,
+            )
         else:
-            raise Exception('Missing infnet parameter, options are zeroshot and combined')
+            raise Exception(
+                "Missing infnet parameter, options are zeroshot and combined"
+            )
 
         if label_size != 0:
             self.label_classification = nn.Linear(n_components, label_size)
@@ -60,8 +85,7 @@ class DecoderNetwork(nn.Module):
         # \mu_1k = log \alpha_k + 1/K \sum_i log \alpha_i;
         # \alpha = 1 \forall \alpha
         topic_prior_mean = 0.0
-        self.prior_mean = torch.tensor(
-            [topic_prior_mean] * n_components)
+        self.prior_mean = torch.tensor([topic_prior_mean] * n_components)
         if torch.cuda.is_available():
             self.prior_mean = self.prior_mean.cuda()
         if self.learn_priors:
@@ -69,9 +93,8 @@ class DecoderNetwork(nn.Module):
 
         # \Sigma_1kk = 1 / \alpha_k (1 - 2/K) + 1/K^2 \sum_i 1 / \alpha_k;
         # \alpha = 1 \forall \alpha
-        topic_prior_variance = 1. - (1. / self.n_components)
-        self.prior_variance = torch.tensor(
-            [topic_prior_variance] * n_components)
+        topic_prior_variance = 1.0 - (1.0 / self.n_components)
+        self.prior_variance = torch.tensor([topic_prior_variance] * n_components)
         if torch.cuda.is_available():
             self.prior_variance = self.prior_variance.cuda()
         if self.learn_priors:
@@ -91,7 +114,7 @@ class DecoderNetwork(nn.Module):
     @staticmethod
     def reparameterize(mu, logvar):
         """Reparameterize the theta distribution."""
-        std = torch.exp(0.5*logvar)
+        std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         return eps.mul(std).add_(mu)
 
@@ -102,18 +125,18 @@ class DecoderNetwork(nn.Module):
         posterior_sigma = torch.exp(posterior_log_sigma)
 
         # generate samples from theta
-        theta = F.softmax(
-            self.reparameterize(posterior_mu, posterior_log_sigma), dim=1)
+        theta = F.softmax(self.reparameterize(posterior_mu, posterior_log_sigma), dim=1)
         theta = self.drop_theta(theta)
 
         # prodLDA vs LDA
-        if self.model_type == 'prodLDA':
+        if self.model_type == "prodLDA":
             # in: batch_size x input_size x n_components
             word_dist = F.softmax(
-                self.beta_batchnorm(torch.matmul(theta, self.beta)), dim=1)
+                self.beta_batchnorm(torch.matmul(theta, self.beta)), dim=1
+            )
             # word_dist: batch_size x input_size
             self.topic_word_matrix = self.beta
-        elif self.model_type == 'LDA':
+        elif self.model_type == "LDA":
             # simplex constrain on Beta
             beta = F.softmax(self.beta_batchnorm(self.beta), dim=1)
             self.topic_word_matrix = beta
@@ -129,17 +152,52 @@ class DecoderNetwork(nn.Module):
         if labels is not None:
             estimated_labels = self.label_classification(theta)
 
-        return self.prior_mean, self.prior_variance, \
-            posterior_mu, posterior_sigma, posterior_log_sigma, word_dist, estimated_labels
+        return (
+            self.prior_mean,
+            self.prior_variance,
+            posterior_mu,
+            posterior_sigma,
+            posterior_log_sigma,
+            word_dist,
+            estimated_labels,
+        )
+
+    def get_posterior(self, x, x_bert, labels=None):
+        """Get posterior distribution."""
+        # batch_size x n_components
+        posterior_mu, posterior_log_sigma = self.inf_net(x, x_bert, labels)
+
+        return posterior_mu, posterior_log_sigma
 
     def get_theta(self, x, x_bert, labels=None):
         with torch.no_grad():
             # batch_size x n_components
-            posterior_mu, posterior_log_sigma = self.inf_net(x, x_bert, labels)
-            #posterior_sigma = torch.exp(posterior_log_sigma)
+            posterior_mu, posterior_log_sigma = self.get_posterior(x, x_bert, labels)
+            # posterior_sigma = torch.exp(posterior_log_sigma)
 
             # generate samples from theta
             theta = F.softmax(
-                self.reparameterize(posterior_mu, posterior_log_sigma), dim=1)
+                self.reparameterize(posterior_mu, posterior_log_sigma), dim=1
+            )
 
             return theta
+
+    def get_sample(self, posterior_mu, posterior_log_sigma):
+        with torch.no_grad():
+            # generate samples from theta
+            theta = F.softmax(
+                self.reparameterize(posterior_mu, posterior_log_sigma), dim=1
+            )
+
+            return theta
+
+    def sample(self, posterior_mu, posterior_log_sigma, n_samples: int = 20):
+        with torch.no_grad():
+            posterior_mu = posterior_mu.unsqueeze(0).repeat(n_samples, 1, 1)
+            posterior_log_sigma = posterior_log_sigma.unsqueeze(0).repeat(n_samples, 1, 1)
+            # generate samples from theta
+            theta = F.softmax(
+                self.reparameterize(posterior_mu, posterior_log_sigma), dim=-1
+            )
+
+            return theta.mean(dim=0)
